36743e27939ded2676105a7fa2ed5694f447190a
Mark Zhan <markzhann@gmail.com>
2015-02-03 22:02:42 +0800

    fix test

--- test/test.ip.js
+++ test/test.ip.js
@@ -1,16 +1,16 @@
 'use strict';
 var test = require('ava');
-var ip = require('../lib/ip.js');
+var re = require('../');
 
 test(function (t) {
-  t.assert(ip.is('192.168.0.1'));
-  t.assert(ip.is('1:2:3:4:5:6:7:8'));
-  t.assert(!ip.is('unicorn 192.168.0.1'));
-  t.assert(ip.v4('192.168.0.1'));
-  t.assert(!ip.v6('192.168.0.1'));
-  t.assert(ip.v6('1:2:3:4:5:6:7:8'));
-  t.assert(!ip.v4('1:2:3:4:5:6:7:8'));
-  t.assert(ip.contain('unicorn 192.168.0.1'));
-  t.assert(ip.contain('unicorn 1:2:3:4:5:6:7:8'));
-  t.assert(ip.match('unicorn 192.168.0.1 cake 1:2:3:4:5:6:7:8 rainbow'));
+  t.assert(re.ip.is('192.168.0.1'));
+  t.assert(re.ip.is('1:2:3:4:5:6:7:8'));
+  t.assert(!re.ip.is('unicorn 192.168.0.1'));
+  t.assert(re.ip.v4('192.168.0.1'));
+  t.assert(!re.ip.v6('192.168.0.1'));
+  t.assert(re.ip.v6('1:2:3:4:5:6:7:8'));
+  t.assert(!re.ip.v4('1:2:3:4:5:6:7:8'));
+  t.assert(re.ip.contain('unicorn 192.168.0.1'));
+  t.assert(re.ip.contain('unicorn 1:2:3:4:5:6:7:8'));
+  t.assert(re.ip.match('unicorn 192.168.0.1 cake 1:2:3:4:5:6:7:8 rainbow'));
 });
